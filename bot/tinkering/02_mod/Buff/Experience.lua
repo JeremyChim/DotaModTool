f0ed6a6de96f87b43665d0099f7002343700cbe3
JeremyChim <jer888chim@outlook.com>
2026-09-05 22:00:15 +0800

    update

--- bot/tinkering/02_mod/Buff/Experience.lua
+++ bot/tinkering/02_mod/Buff/Experience.lua
@@ -5,8 +5,69 @@ then
     XP = {}
 end
 
+-- local XPNeeded = {
+--     [1]  = 240,
+--     [2]  = 400,
+--     [3]  = 520,
+--     [4]  = 600,
+--     [5]  = 680,
+--     [6]  = 760,
+--     [7]  = 800,
+--     [8]  = 900,
+--     [9]  = 1000,
+--     [10] = 1100,
+--     [11] = 1200,
+--     [12] = 1300,
+--     [13] = 1400,
+--     [14] = 1500,
+--     [15] = 1600,
+--     [16] = 1700,
+--     [17] = 1800,
+--     [18] = 1900,
+--     [19] = 2000,
+--     [20] = 2200,
+--     [21] = 2400,
+--     [22] = 2600,
+--     [23] = 2800,
+--     [24] = 3000,
+--     [25] = 4000,
+--     [26] = 5000,
+--     [27] = 6000,
+--     [28] = 7000,
+--     [29] = 7500,
+--     [30] = 0,
+-- }
+
+-- -- just eyeballed
+-- function XP.UpdateXP(bot, nTeam)
+--     local gameTime = Helper.DotaTime()
+--     local botPos = Helper.GetPosition(bot, nTeam)
+
+--     local xp = 1
+--     if botPos == 1 then
+--         xp = 5
+--     elseif botPos == 2 then
+--         xp = 4
+--     elseif botPos == 3 then
+--         xp = 3
+--     end
+
+--     if bot:IsAlive() and gameTime > 0 and math.floor(gameTime) % 2 == 0 then
+--         bot:AddExperience(math.floor(xp), 0, false, true)
+--     end
+-- end
+
 function XP.UpdateXP(bot, xp)
-	bot:AddExperience(xp, 0, false, true, bot:GetPlayerOwnerID())
+    local gameTime = Helper.DotaTime()
+    if gameTime > 20 * 60 then
+        xp = xp * 2
+    end
+
+    if not bot:IsAlive() then
+        xp = xp * 3
+    end
+
+    bot:AddExperience(xp, 0, false, true, bot:GetPlayerOwnerID())
 end
 
 return XP
