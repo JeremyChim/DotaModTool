eaa1f5f477fc700c258c428de4159e0c08a3b827
Jer <Jer@88.com>
2026-09-07 17:22:38 +0800

    update

--- tool/build.py
+++ tool/build.py
@@ -21,7 +21,7 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent
 ENTRY_POINT = PROJECT_ROOT / "win.py"
 RELEASE_DIR = PROJECT_ROOT / "release"
 RESOURCE_DIRS = ("bot", "npc", "gi", "vpk")
-RESOURCE_FILES = ("config.json",)
+RESOURCE_FILES = ("config.json", "name.json")
 DEFAULT_NAME = "DotaModTool"
 REPLACE_RETRIES = 10
 
