f8b702817ee8a8815e47186c4a7d3eb7aa439003
JeremyChim <jer888chim@outlook.com>
2026-09-26 17:29:44 +0800

    update

--- bot/tinkering/02_mod/Buff/Buff.lua
+++ bot/tinkering/02_mod/Buff/Buff.lua
@@ -1,12 +1,13 @@
 dofile('bots/Buff/Timers')
 dofile('bots/Buff/Experience')
 dofile('bots/Buff/GPM')
-dofile('bots/Buff/Attributes')
 dofile('bots/Buff/NeutralItems')
 dofile('bots/Buff/Helper')
 dofile('bots/Buff/Towers')
 dofile('bots/Buff/Facets')
 dofile('bots/Buff/Spells')
+
+local SpellMode = require('bots/Buff/SpellsMore')
 
 if Buff == nil
 then
@@ -15,14 +16,12 @@ end
 
 if BuffEnabled == nil then BuffEnabled = false end
 
-local botTable = {
-    [DOTA_TEAM_GOODGUYS]    = {},
-    [DOTA_TEAM_BADGUYS]     = {}
+local HeroTable = {
+    bot = { [DOTA_TEAM_GOODGUYS] = {}, [DOTA_TEAM_BADGUYS] = {} },
+    all = { [DOTA_TEAM_GOODGUYS] = {}, [DOTA_TEAM_BADGUYS] = {} },
 }
 
-function Buff:AddBotsToTable()
-    -- with 7.38, player IDs are not 9<= anymore; when no human on either side
-    -- 2 rad, 3 dire
+function Buff:AddPlayersToTable()
     for _, team in pairs({DOTA_TEAM_GOODGUYS, DOTA_TEAM_BADGUYS}) do
         local playerCount = PlayerResource:GetPlayerCountForTeam(team)
         for j = 1, playerCount do
@@ -30,83 +29,46 @@ function Buff:AddBotsToTable()
             local player = PlayerResource:GetPlayer(playerID)
             if player then
                 local hero = player:GetAssignedHero()
-                -- just do how we're counting bots from below
-                if hero and PlayerResource:GetSteamID(playerID) == PlayerResource:GetSteamID(playerCount + 1) then
-                    -- -- check if these heroes do not have the specified spell
-                    -- -- valve fixed this a while ago
-                    -- if string.find(hero:GetUnitName(), 'life_stealer') then
-                    --     if not hero:HasAbility('life_stealer_rage') then
-                    --         AddAbilityToHero(hero, 'life_stealer_rage')
-                    --     end
-                    -- elseif string.find(hero:GetUnitName(), 'faceless_void') then
-                    --     if not hero:HasAbility('faceless_void_chronosphere') then
-                    --         AddAbilityToHero(hero, 'faceless_void_chronosphere')
-                    --     end
-                    -- end
-
-                    table.insert(botTable[team], hero)
+                if hero then
+                    if PlayerResource:GetSteamID(playerID) == PlayerResource:GetSteamID(playerCount + 1) then
+                        table.insert(HeroTable.bot[team], hero)
+                    end
+                    table.insert(HeroTable.all[team], hero)
                 end
             end
         end
     end
 end
 
--- fix Lifestealer and Faceless Void missing ultimates due to facet
--- if anyone is using this vscript with another bot script, you might need to change some stuff to work with it (ie. level up list)
-function AddAbilityToHero(hero, hAbilityName)
-    local missingSpell = nil
-    local abilityCount = hero:GetAbilityCount()
-    hero:AddAbility(hAbilityName)
-
-    for i = abilityCount - 1, 0, -1
-    do
-        local ability = hero:GetAbilityByIndex(i)
-        if ability then
-            if ability:GetAbilityName() == hAbilityName
-            then
-                missingSpell = ability
-            end
-
-            if ability:GetAbilityName() == 'generic_hidden' and missingSpell then
-                hero:SwapAbilities(missingSpell:GetAbilityName(), ability:GetAbilityName(), true, false)
-                break
-            end
+local function MergeTables(...)
+    local result = {}
+    for _, tbl in ipairs({...}) do
+        for _, v in pairs(tbl) do
+            table.insert(result, v)
         end
     end
 
-    -- swap lifestealer's spells to their correct slots for sAbilityLevelUpList
-    if hero:GetUnitName() == 'npc_dota_hero_life_stealer' then
-        local ability_1 = 'life_stealer_rage'
-        local ability_2 = 'life_stealer_open_wounds'
-        local ability_3 = 'life_stealer_ghoul_frenzy'
-
-        if hero:HasAbility(ability_1) and hero:HasAbility(ability_2) and hero:HasAbility(ability_3) then
-            hero:SwapAbilities(ability_2, ability_3, true, true)
-            hero:SwapAbilities(ability_1, ability_3, true, true)
-        end
-    end
+    return result
 end
 
-local BotCount = 0
-local SelectedHeroCount = 0
-local function CheckBotCount()
-    BotCount = 0
-    SelectedHeroCount = 0
-    local playerCount = PlayerResource:GetPlayerCount()
+local function GetHeroes(nHeroTable)
+    return MergeTables( nHeroTable[DOTA_TEAM_GOODGUYS], nHeroTable[DOTA_TEAM_BADGUYS] )
+end
 
-    for playerID = 0, playerCount - 1 do
-        if PlayerResource:GetSteamID(playerID) == PlayerResource:GetSteamID(playerCount + 1) then
-            if PlayerResource:GetSelectedHeroName(playerID) ~= '' then
-                SelectedHeroCount = SelectedHeroCount + 1
-            end
-
-            BotCount = BotCount + 1
-        end
-    end
+local function CheckHeroTables()
+    HeroTable.bot[DOTA_TEAM_GOODGUYS] = {}
+    HeroTable.bot[DOTA_TEAM_BADGUYS]  = {}
+    HeroTable.all[DOTA_TEAM_GOODGUYS] = {}
+    HeroTable.all[DOTA_TEAM_BADGUYS]  = {}
+    Buff:AddPlayersToTable()
 end
 
 -- script flags
 local bBuffFlags = {
+    -- Needed
+    spellUsage      = true,
+    bountyPickUpFix = true,
+
     -- General
     facets = {
         change  = false,  -- Set to 'false' to disable changing (a) facet/s (See /Facets.lua for the heroes).
@@ -119,13 +81,21 @@ local bBuffFlags = {
         radiant = true, -- Set to 'false' to disable Radiant bots receiving neutral items.
         dire    = true, -- Set to 'false' to disable Dire bots receiving neutral items.
     },
-    manga_regen = {
-        radiant = false, -- Set to 'false' to disable aiding Radiant bots' receiving added mana regen.
-        dire    = false, -- Set to 'false' to disable aiding Dire bots' receiving added mana regen.
+    mana_regen = {
+        radiant = true, -- Set to 'false' to disable aiding Radiant bots' receiving added mana regen.
+        dire    = true, -- Set to 'false' to disable aiding Dire bots' receiving added mana regen.
     },
     gpm = {
         radiant = true, -- Set to 'false' to disable Radiant bots receiving a Gold boost.
         dire    = true, -- Set to 'false' to disable Dire bots receiving a Gold boost.
+    },
+    morespells = {
+        enable  = false, -- Set to 'false' to disable all playes getting more spells.
+                        -- Proper LoD would be too tedious; pseudo.
+                        -- Can be laggy.
+        humans  = true, -- Set to 'false' to disable humans getting more spells.
+                        -- Don't manually upgrade the added spells.
+                        -- Don't hover over the added spell if not cached yet (see Console).
     },
     -- Applies to All Pick only
     xpm = {
@@ -139,87 +109,83 @@ function Buff:Init()
         GameRules:SendCustomMessage('Buff mode enabled!', 0, 0)
         BuffEnabled = true
     end
-
     Timers:CreateTimer(function()
-        -- CheckBotCount()
-        -- if BotCount ~= SelectedHeroCount then return 1 end
+        CheckHeroTables()
 
-        -- just refresh instead of above
-        botTable[DOTA_TEAM_GOODGUYS] = {}
-        botTable[DOTA_TEAM_BADGUYS] = {}
-        Buff:AddBotsToTable()
-        local TeamRadiant = botTable[DOTA_TEAM_GOODGUYS]
-        local TeamDire = botTable[DOTA_TEAM_BADGUYS]
-        local hHeroList = {}
+        local nBotHeroes = GetHeroes(HeroTable.bot)
+        local nAllHeroes = GetHeroes(HeroTable.all)
 
-        if bBuffFlags.facets.change and GameRules:GetDOTATime(false, true) < 0 then
-            for _, h in pairs(TeamRadiant) do table.insert(hHeroList, h) end
-            for _, h in pairs(TeamDire) do table.insert(hHeroList, h) end
-
-            for _, hero in pairs(hHeroList) do
-                if hero.facet_flag == nil then hero.facet_flag = false end
-                if hero.facet_flag == false then
+        for _, hero in pairs(nAllHeroes) do
+            -- Facet Change
+            if bBuffFlags.facets.change and GameRules:GetDOTATime(false, true) < 0 then
+                if not hero.facet_flag then
                     F.ChangeHeroFacet(hero)
                 end
             end
         end
 
-        -- Spell Usage
-        for _, h in pairs(TeamRadiant) do table.insert(hHeroList, h) end
-        for _, h in pairs(TeamDire) do table.insert(hHeroList, h) end
-        for _, hero in pairs(hHeroList) do
-            if hero and S.AbilityUsageHeroList[hero:GetUnitName()] then
-                S.AbilityUsageThink(hero)
-            end
-
+        for _, hero in pairs(nBotHeroes) do
             if hero and hero:IsAlive() then
-                S.TryPickUpNearbyBounty(hero)
+                -- Spell Usage
+                if bBuffFlags.spellUsage then
+                    if S.AbilityUsageHeroList[hero:GetUnitName()] then
+                        S.AbilityUsageThink(hero)
+                    end
+                end
+
+                if bBuffFlags.bountyPickUpFix then
+                    S.TryPickUpNearbyBounty(hero)
+                end
+            end
+        end
+
+        -- More Spells
+        if bBuffFlags.morespells.enable and GameRules:State_Get() >= DOTA_GAMERULES_STATE_PRE_GAME then
+            local hHeroList = (bBuffFlags.morespells.humans and nAllHeroes) or nBotHeroes
+            for _, hero in pairs(hHeroList) do
+                if hero then
+                    SpellMode.InitMoreSpells(hero, HeroTable.all)
+                end
             end
         end
 
         if GameRules:GetDOTATime(false, false) > 0 then
             -- Mana
-            hHeroList = {}
-            if bBuffFlags.manga_regen.radiant then
-                for _, h in pairs(TeamRadiant) do
-                    table.insert(hHeroList, h)
-                end
-            end
-            if bBuffFlags.manga_regen.dire then
-                for _, h in pairs(TeamDire) do
-                    table.insert(hHeroList, h)
-                end
-            end
+            for _, hero in pairs(nBotHeroes) do
+                if  hero then
+                    if (bBuffFlags.mana_regen.radiant and hero:GetTeam() == DOTA_TEAM_GOODGUYS)
+                    or (bBuffFlags.mana_regen.dire and hero:GetTeam() == DOTA_TEAM_BADGUYS)
+                    then
+                        local nManaCost = 0
+                        for i = 0, hero:GetAbilityCount() - 1 do
+                            local hAbility = hero:GetAbilityByIndex(i)
+                            if hAbility then nManaCost = nManaCost + hAbility:GetManaCost(-1) end
+                        end
 
-            for _, hero in pairs(hHeroList) do
-                local nManaCost = 0
-                for i = 0, hero:GetAbilityCount() - 1 do
-                    local hAbility = hero:GetAbilityByIndex(i)
-                    if hAbility then nManaCost = nManaCost + hAbility:GetManaCost(-1) end
-                end
+                        local idx = {0, 1, 2, 3, 4, 5, 15, 16}
+                        for _, i in ipairs(idx) do
+                            local hItem = hero:GetItemInSlot(i)
+                            if hItem then nManaCost = nManaCost + hItem:GetManaCost(-1) end
+                        end
 
-                local idx = {0, 1, 2, 3, 4, 5, 15, 16}
-                for _, i in ipairs(idx) do
-                    local hItem = hero:GetItemInSlot(i)
-                    if hItem then nManaCost = nManaCost + hItem:GetManaCost(-1) end
+                        hero:SetBaseManaRegen(((math.max(nManaCost - hero:GetMana(), 0)) / 30))
+                    end
                 end
-
-                hero:SetBaseManaRegen(((math.max(nManaCost - hero:GetMana(), 0)) / 30))
             end
 
             -- Towers
-            -- T.HandleTowerBuff(DOTA_TEAM_GOODGUYS, bBuffFlags.towers.radiant)
-            -- T.HandleTowerBuff(DOTA_TEAM_BADGUYS, bBuffFlags.towers.dire)
+            T.HandleTowerBuff(DOTA_TEAM_GOODGUYS, bBuffFlags.towers.radiant)
+            T.HandleTowerBuff(DOTA_TEAM_BADGUYS, bBuffFlags.towers.dire)
 
-            hHeroList = {}
             -- Neutral Items
+            local hHeroList = {}
             if bBuffFlags.neutrals.radiant then
-                for _, h in pairs(TeamRadiant) do
+                for _, h in pairs(HeroTable.bot[DOTA_TEAM_GOODGUYS]) do
                     table.insert(hHeroList, h)
                 end
             end
             if bBuffFlags.neutrals.dire then
-                for _, h in pairs(TeamDire) do
+                for _, h in pairs(HeroTable.bot[DOTA_TEAM_BADGUYS]) do
                     table.insert(hHeroList, h)
                 end
             end
@@ -228,48 +194,34 @@ function Buff:Init()
 
             -- Gold and Experience For Player
             local me = PlayerResource:GetPlayer(0):GetAssignedHero()
-            GPM.UpdateBotGold(me, 4)
-            XP.UpdateXP(me, 4)
-            -- GPM.UpdateGoldWhenDeath(me, 500)
-            -- XP.UpdateXPWhenDeath(me, 500)
-            --Attributes.UpdateAttrWhenDeath(me, 20, 20)
+            GPM.UpdateBotGold(me, 100)
+            XP.UpdateXP(me, 100)
 
-            -- Gold and Experience
-            for _, h in pairs(TeamRadiant) do
-                -- if bBuffFlags.gpm.radiant then
-                --     GPM.UpdateBotGold(h, TeamRadiant)
-                -- end
-                GPM.UpdateBotGold(h, 4)
-				XP.UpdateXP(h, 4)
-                -- GPM.UpdateGoldWhenDeath(h, 500)
-                -- XP.UpdateXPWhenDeath(h, 500)
-                --Attributes.UpdateAttrWhenDeath(h, 20, 20)
+            -- Gold
+            for _, hero in pairs(nBotHeroes) do
+                if hero then
+                    local nTeam = hero:GetTeam()
+                    if (bBuffFlags.gpm.radiant and nTeam == DOTA_TEAM_GOODGUYS)
+                    or (bBuffFlags.gpm.dire and nTeam == DOTA_TEAM_BADGUYS)
+                    then
+                        GPM.UpdateBotGold(hero, 500)
+                    end
+                end
             end
 
-            for _, h in pairs(TeamDire) do
-                -- if bBuffFlags.gpm.dire then
-                --     GPM.UpdateBotGold(h, TeamDire)
-                -- end
-                GPM.UpdateBotGold(h, 8)
-				XP.UpdateXP(h, 8)
-                -- GPM.UpdateGoldWhenDeath(h, 750)
-                -- XP.UpdateXPWhenDeath(h, 750)
-                --Attributes.UpdateAttrWhenDeath(h, 20, 20)
+            -- Experience
+            if not Helper.IsTurboMode() then
+                for _, hero in pairs(nBotHeroes) do
+                    if hero then
+                        local nTeam = hero:GetTeam()
+                        if (bBuffFlags.xpm.radiant and nTeam == DOTA_TEAM_GOODGUYS)
+                        or (bBuffFlags.xpm.dire and nTeam == DOTA_TEAM_BADGUYS)
+                        then
+                            XP.UpdateXP(hero, 1000)
+                        end
+                    end
+                end
             end
-
-            -- if not Helper.IsTurboMode() then
-            --     for _, h in pairs(TeamRadiant) do
-            --         if bBuffFlags.xpm.radiant then
-            --             XP.UpdateXP(h, TeamRadiant)
-            --         end
-            --     end
-
-            --     for _, h in pairs(TeamDire) do
-            --         if bBuffFlags.xpm.dire then
-            --             XP.UpdateXP(h, TeamDire)
-            --         end
-            --     end
-            -- end
         end
 
         return 1
